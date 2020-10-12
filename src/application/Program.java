14affd2ce843237cd6bde1d3a0f82b6832d65171
gusfelipes <gusfelipes@gmail.com>
2020-10-12 15:02:21 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -24,7 +25,7 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println();
 		System.out.println("--- Test 03: Seller FindAll ---");
 		list = sellerDao.findAll();
@@ -32,6 +33,11 @@ public class Program {
 			System.out.println(obj);
 		}
 
+		System.out.println();
+		System.out.println("--- Test 04: Seller insert ---");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted, New Seller id: " + newSeller.getId());
 	}
 
 }
