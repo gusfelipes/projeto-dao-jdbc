01fa1684ef9d83d36c141498966135b5cfede5d4
gusfelipes <gusfelipes@gmail.com>
2020-10-12 15:33:31 -0300

    findById implementation

--- src/application/ProgramDepartment.java
+++ src/application/ProgramDepartment.java
@@ -2,14 +2,22 @@ package application;
 
 import java.util.Scanner;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
 public class ProgramDepartment {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+		
+		DepartmentDao departmentDao = DaoFactory.createDeparmentDao();
 
 
 		System.out.println("--- Test 01: Department FindById ---");
+		Department dep = departmentDao.findById(1);
+		System.out.println(dep);
 
 		System.out.println();
 		System.out.println("--- Test 02: Seller FindAll ---");
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,13 +1,18 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
-public class DepartmentDaoJDBC implements DepartmentDao{
-	
+public class DepartmentDaoJDBC implements DepartmentDao {
+
 	private Connection conn;
 
 	public DepartmentDaoJDBC(Connection conn) {
@@ -17,25 +22,47 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 	@Override
 	public void insert(Department obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Department obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Department findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+
+			st = conn.prepareStatement("SELECT * FROM department WHERE Id=?");
+			st.setInt(1, id);
+
+			rs = st.executeQuery();
+			if (rs.next()) {
+				Department dep = new Department();
+				dep.setId(rs.getInt("Id"));
+				dep.setName(rs.getString("Name"));
+				return dep;
+			} else {
+				return null;
+			}
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
