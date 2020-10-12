824a9f21eb882d914941a10493a5aa41391b1406
gusfelipes <gusfelipes@gmail.com>
2020-10-12 15:26:58 -0300

    DepartmentDaoJdbc, DaoFactory, ProgramDeparment

---
+++ src/application/ProgramDepartment.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.Scanner;
+
+public class ProgramDepartment {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+
+		System.out.println("--- Test 01: Department FindById ---");
+
+		System.out.println();
+		System.out.println("--- Test 02: Seller FindAll ---");
+
+		System.out.println();
+		System.out.println("--- Test 03: Seller insert ---");
+		
+		System.out.println();
+		System.out.println("--- Test 04: Seller update ---");
+		
+		System.out.println();
+		System.out.println("--- Test 05: Seller delete ---");
+		
+		sc.close();
+	}
+
+}
